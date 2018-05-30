e40b761330f67aa6c92d65ef295f90f91216b2bb
Vinícius de Oliveira Souza <viniciuso970@gmail.com>
2018-05-30 23:33:36 +0000

    Alterando gitignore e inserindo script do banco

--- Projeto/sql/supermaps.sql
+++ Projeto/sql/supermaps.sql
@@ -1,11 +1,11 @@
 -- phpMyAdmin SQL Dump
--- version 4.7.0
+-- version 4.7.9
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: 27-Maio-2018 às 21:01
--- Versão do servidor: 10.1.25-MariaDB
--- PHP Version: 7.1.7
+-- Generation Time: May 31, 2018 at 01:32 AM
+-- Server version: 10.1.31-MariaDB
+-- PHP Version: 7.2.3
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET AUTOCOMMIT = 0;
@@ -19,13 +19,13 @@ SET time_zone = "+00:00";
 /*!40101 SET NAMES utf8mb4 */;
 
 --
--- Database: `saladeaula`
+-- Database: `supermaps`
 --
 
 -- --------------------------------------------------------
 
 --
--- Estrutura da tabela `mapa`
+-- Table structure for table `mapa`
 --
 
 CREATE TABLE `mapa` (
@@ -34,10 +34,31 @@ CREATE TABLE `mapa` (
   `altura` int(11) NOT NULL DEFAULT '10'
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
+--
+-- Dumping data for table `mapa`
+--
+
+INSERT INTO `mapa` (`id`, `largura`, `altura`) VALUES
+(0, 10, 10);
+
 -- --------------------------------------------------------
 
 --
--- Estrutura da tabela `usuario`
+-- Table structure for table `prateleira`
+--
+
+CREATE TABLE `prateleira` (
+  `id` int(11) NOT NULL,
+  `andar` int(11) NOT NULL,
+  `produtoAndar` int(11) NOT NULL,
+  `x` int(11) DEFAULT NULL,
+  `y` int(11) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `usuario`
 --
 
 CREATE TABLE `usuario` (
@@ -48,12 +69,17 @@ CREATE TABLE `usuario` (
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
--- Extraindo dados da tabela `usuario`
+-- Dumping data for table `usuario`
 --
 
 INSERT INTO `usuario` (`id`, `usuario`, `email`, `senha`) VALUES
+(0, 'vinicius123', 'viniciuso970@gmail.com', 'vinicius123'),
 (1, 'vinicius', 'vinicius@mail.com', 'vinicius'),
 (2, 'professor', 'professor@mail.com', 'professor');
+
+--
+-- Indexes for dumped tables
+--
 
 --
 -- Indexes for table `mapa`
@@ -62,9 +88,19 @@ ALTER TABLE `mapa`
   ADD PRIMARY KEY (`id`);
 
 --
+-- Indexes for table `prateleira`
+--
+ALTER TABLE `prateleira`
+  ADD PRIMARY KEY (`id`);
+
+--
 -- Indexes for table `usuario`
 --
 ALTER TABLE `usuario`
   ADD PRIMARY KEY (`id`),
   ADD UNIQUE KEY `usuario` (`usuario`);
+COMMIT;
 
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
